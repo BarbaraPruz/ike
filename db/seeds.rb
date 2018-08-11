98a38b25fc45af492268a1cfe6e161af35a3db64
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-08-10 22:20:45 -0700

    Add user model, migration, seeds

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+User.create(:name => "Roger", :email => "roger@domain.com", :password => "test")
+User.create(:name => "Serena", :email => "serena@domain.com", :password => "test")
+User.create(:name => "John", :email => "johnmc@domain.com", :password => "test", :admin => "true")
