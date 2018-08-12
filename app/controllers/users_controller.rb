1b75d4994266ae9894fdfe70bdb7e0884e243c0c
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-08-11 17:25:47 -0700

    Add signup form with validation and user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,18 @@ class UsersController < ApplicationController
     end
 
     def create
-        raise params.inspect
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            # to do: login and then redirect_to 'home'
+        else
+            render :new
+        end
     end
 
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
