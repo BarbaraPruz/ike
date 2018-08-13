cc04760fb2742edf2a9796a6e5e9eb871ac71b66
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-08-12 18:39:06 -0700

    improve create session error handling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,10 @@ require 'pry'
 class SessionsController < ApplicationController
 
     def create
-        temp_user = User.new(email: params[:email])
         @user = User.find_by(email: params[:email])
         if !@user
             flash[:alert] = "User Email not found."
-            @user = temp_user  # so user can see what they entered
+            @user = User.new(email: params[:email])        
             render :welcome
         elsif @user.authenticate(params[:password])
             session[:user_id] = @user.id
