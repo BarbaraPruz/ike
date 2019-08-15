55eaed798f6e362978a9f9230b0854af368b141a
BarbaraPruz <barbara.prusiewicz@gmail.com>
2019-08-14 19:45:58 -1000

    Rubocop config.  Cleanup rubocop messages for migrations and routes (in progress)

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,26 @@
 Rails.application.routes.draw do
 
-    # Users and sessions 
-    root 'application#welcome', as: :welcome
+  # Users and sessions 
+  root 'application#welcome', as: :welcome
 
-    get '/login' => 'application#welcome'
-    post '/login' => 'sessions#create'
-    delete '/logout' => 'sessions#destroy'
-    get "/auth/:provider/callback" => "sessions#create"
+  get '/login' => 'application#welcome'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  get "/auth/:provider/callback" => 'sessions#create'
 
-    resources :users
+  resources :users
 
-    resources :topics, only: [:index, :edit, :show, :update, :destroy]
-    get '/topics/admin' => 'topics#admin'
-    get '/topics/:id/articles/:article_id' => 'topics#topic_articles', as: :topic_articles
- 
-    # article and comments and bookmarks
-    resources :articles do
-        resources :comments, only: [:new, :create]
-        resources :bookmarks, only: [:new, :create, :destroy]
-    end    
-    post '/articles/:article_id/like' => 'likes#create', as: :like_article
+  resources :topics, only: [:index, :edit, :show, :update, :destroy]
+  get '/topics/admin' => 'topics#admin'
+  get '/topics/:id/articles/:article_id' => 'topics#topic_articles', as: :topic_articles
 
-    # miscellaneous
-    get '/about' => 'static#about'
+  # article and comments and bookmarks
+  resources :articles do
+    resources :comments, only: [:new, :create]
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
+  post '/articles/:article_id/like' => 'likes#create', as: :like_article
+
+  # miscellaneous
+  get '/about' => 'static#about'
 end
