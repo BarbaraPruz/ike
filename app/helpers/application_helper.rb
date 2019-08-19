7686675d6377192a66b516088bf991685e04ea37
BarbaraPruz <barbara.prusiewicz@gmail.com>
2019-08-18 15:20:07 -1000

    Fix rubocop warnings for application helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
+# Application Helpers used by views and/or controllers
 module ApplicationHelper
   def home_path
     return user_path(session[:user_id]) if logged_in?
-    
+
     welcome_path
   end
 
@@ -12,11 +13,13 @@ module ApplicationHelper
   end
 
   def show_error_messages(ar_object)
-    html = '<div id="error_explanation"><p>oops, there seem to be some problems:</p><ul>'
+    html = '<div id="error_explanation">'\
+      '<p>oops, there seem to be some problems:</p>'\
+      '<ul>'
     ar_object.errors.full_messages.each do |message|
       html += "li>#{message}</li>"
     end
-    html += '<ul></div>'
+    html += '<ul></ul>'
     html.html_safe
   end
 
@@ -28,12 +31,17 @@ module ApplicationHelper
               { value: 'likes', display: 'Likes' },
               { value: 'updated_at', display: 'Latest Update' }]
 
+    html = build_html_select(fields, sort_field)
+    html.html_safe
+  end
+
+  def build_html_select(fields, sort_field)
     html = "<select name='sort' id='sort'>"
     fields.each do |field|
       html += "<option #{sort_selected_option(field[:value], sort_field)} "
       html += " value=#{field[:value]}>#{field[:display]}</option>"
     end
-    html.html_safe
+    html
   end
 
   def sort_selected_option(field, sort_field)
